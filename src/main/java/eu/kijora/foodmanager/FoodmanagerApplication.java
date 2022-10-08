e05d0c9ddfeb2073b4b1a9cc822086f70ffe2462
Jedrzej <jedrek.kijora@gmail.com>
2022-10-08 15:59:32 +0200

    init

---
+++ src/main/java/eu/kijora/foodmanager/FoodmanagerApplication.java
@@ -0,0 +1,13 @@
+package eu.kijora.foodmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmanagerApplication.class, args);
+	}
+
+}
